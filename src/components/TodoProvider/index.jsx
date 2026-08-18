28b16edfa90ed03966653eec34a36295e9bd4605
andretmsoares <andre.tharssys.marques.soares@ccc.ufcg.edu.br>
2026-08-18 16:39:31 -0300

    etapa 3 curso

--- src/components/TodoProvider/index.jsx
+++ src/components/TodoProvider/index.jsx
@@ -1,45 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoContext from "./TodoContext";
 
+const TODOS = 'todos'
+
 export function TodoProvider({ children }) {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            description: "JSX e componentes",
-            completed: false,
-            createdAt: "2022-10-31"
-        },
-        {
-            id: 2,
-            description: "Props, state e hooks",
-            completed: true,
-            createdAt: "2022-10-31"
-        },
-        {
-            id: 3,
-            description: "Ciclo de vida dos componentes",
-            completed: false,
-            createdAt: "2022-10-31"
-        },
-        {
-            id: 4,
-            description: "Testes unitários com Jest",
-            completed: false,
-            createdAt: "2022-10-31"
-        },
-        {
-            id: 5,
-            description: "Controle de inputs e formulários controlados",
-            completed: true,
-            createdAt: "2022-10-31"
-        },
-        {
-            id: 6,
-            description: "Rotas dinâmicas",
-            completed: true,
-            createdAt: "2022-10-31"
-        }
-    ]);
+
+    const savedTodos = localStorage.getItem(TODOS)
+
+    const [todos, setTodos] = useState(savedTodos ? JSON.parse(savedTodos) : [])
+
+    useEffect (() => {
+        localStorage.setItem(TODOS, JSON.stringify(todos))
+    }, [todos])
 
     const addTodo = (formData) => {
         const description = formData.get('description');
